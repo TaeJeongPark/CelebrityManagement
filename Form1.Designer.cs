a74fab3ca58bc255ab2d3f4a0598ed18e7aa4fbe
TaeJeongPark <ksmpjc99@gmail.com>
2023-10-13 16:03:38 +0900

    초기 커밋.

---
+++ Form1.Designer.cs
@@ -0,0 +1,346 @@
+﻿namespace Project1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.tb_name = new System.Windows.Forms.TextBox();
+            this.tb_agency = new System.Windows.Forms.TextBox();
+            this.tb_debut = new System.Windows.Forms.TextBox();
+            this.tb_prize = new System.Windows.Forms.TextBox();
+            this.tb_site = new System.Windows.Forms.TextBox();
+            this.tb_image = new System.Windows.Forms.TextBox();
+            this.btn_image = new System.Windows.Forms.Button();
+            this.btn_clear = new System.Windows.Forms.Button();
+            this.btn_save = new System.Windows.Forms.Button();
+            this.btn_update = new System.Windows.Forms.Button();
+            this.btn_delete = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader5 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader6 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.columnHeader7 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(16, 17);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(288, 364);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.label1.Location = new System.Drawing.Point(338, 59);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 18);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "이름";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.label2.Location = new System.Drawing.Point(338, 124);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(62, 18);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "소속사";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.label3.Location = new System.Drawing.Point(338, 190);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(62, 18);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "데뷔일";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.label4.Location = new System.Drawing.Point(338, 260);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(44, 18);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "수상";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.label5.Location = new System.Drawing.Point(337, 332);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(62, 18);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "사이트";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.label6.Location = new System.Drawing.Point(13, 405);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(98, 18);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "이미지경로";
+            this.label6.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // tb_name
+            // 
+            this.tb_name.Location = new System.Drawing.Point(418, 59);
+            this.tb_name.Name = "tb_name";
+            this.tb_name.Size = new System.Drawing.Size(219, 28);
+            this.tb_name.TabIndex = 1;
+            // 
+            // tb_agency
+            // 
+            this.tb_agency.Location = new System.Drawing.Point(418, 124);
+            this.tb_agency.Name = "tb_agency";
+            this.tb_agency.Size = new System.Drawing.Size(219, 28);
+            this.tb_agency.TabIndex = 2;
+            // 
+            // tb_debut
+            // 
+            this.tb_debut.Location = new System.Drawing.Point(418, 190);
+            this.tb_debut.Name = "tb_debut";
+            this.tb_debut.Size = new System.Drawing.Size(219, 28);
+            this.tb_debut.TabIndex = 3;
+            // 
+            // tb_prize
+            // 
+            this.tb_prize.Location = new System.Drawing.Point(418, 260);
+            this.tb_prize.Name = "tb_prize";
+            this.tb_prize.Size = new System.Drawing.Size(219, 28);
+            this.tb_prize.TabIndex = 4;
+            // 
+            // tb_site
+            // 
+            this.tb_site.Location = new System.Drawing.Point(417, 332);
+            this.tb_site.Name = "tb_site";
+            this.tb_site.Size = new System.Drawing.Size(219, 28);
+            this.tb_site.TabIndex = 5;
+            // 
+            // tb_image
+            // 
+            this.tb_image.Location = new System.Drawing.Point(117, 400);
+            this.tb_image.Name = "tb_image";
+            this.tb_image.Size = new System.Drawing.Size(399, 28);
+            this.tb_image.TabIndex = 6;
+            // 
+            // btn_image
+            // 
+            this.btn_image.Location = new System.Drawing.Point(522, 400);
+            this.btn_image.Name = "btn_image";
+            this.btn_image.Size = new System.Drawing.Size(115, 28);
+            this.btn_image.TabIndex = 7;
+            this.btn_image.Text = "불러오기";
+            this.btn_image.UseVisualStyleBackColor = true;
+            this.btn_image.Click += new System.EventHandler(this.btn_image_Click);
+            // 
+            // btn_clear
+            // 
+            this.btn_clear.Location = new System.Drawing.Point(11, 442);
+            this.btn_clear.Name = "btn_clear";
+            this.btn_clear.Size = new System.Drawing.Size(152, 65);
+            this.btn_clear.TabIndex = 8;
+            this.btn_clear.Text = "초기화";
+            this.btn_clear.UseVisualStyleBackColor = true;
+            // 
+            // btn_save
+            // 
+            this.btn_save.Location = new System.Drawing.Point(169, 442);
+            this.btn_save.Name = "btn_save";
+            this.btn_save.Size = new System.Drawing.Size(152, 65);
+            this.btn_save.TabIndex = 9;
+            this.btn_save.Text = "저장";
+            this.btn_save.UseVisualStyleBackColor = true;
+            this.btn_save.Click += new System.EventHandler(this.btn_save_Click);
+            // 
+            // btn_update
+            // 
+            this.btn_update.Location = new System.Drawing.Point(327, 442);
+            this.btn_update.Name = "btn_update";
+            this.btn_update.Size = new System.Drawing.Size(152, 65);
+            this.btn_update.TabIndex = 10;
+            this.btn_update.Text = "수정";
+            this.btn_update.UseVisualStyleBackColor = true;
+            // 
+            // btn_delete
+            // 
+            this.btn_delete.Location = new System.Drawing.Point(485, 442);
+            this.btn_delete.Name = "btn_delete";
+            this.btn_delete.Size = new System.Drawing.Size(152, 65);
+            this.btn_delete.TabIndex = 11;
+            this.btn_delete.Text = "삭제";
+            this.btn_delete.UseVisualStyleBackColor = true;
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4,
+            this.columnHeader7,
+            this.columnHeader5,
+            this.columnHeader6});
+            this.listView1.GridLines = true;
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(11, 525);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(626, 288);
+            this.listView1.TabIndex = 9;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "번호";
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "이름";
+            this.columnHeader2.Width = 100;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "소속사";
+            this.columnHeader3.Width = 100;
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "데뷔";
+            this.columnHeader4.Width = 100;
+            // 
+            // columnHeader5
+            // 
+            this.columnHeader5.Text = "사이트";
+            this.columnHeader5.Width = 100;
+            // 
+            // columnHeader6
+            // 
+            this.columnHeader6.Text = "파일경로";
+            this.columnHeader6.Width = 200;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // columnHeader7
+            // 
+            this.columnHeader7.Text = "수상";
+            this.columnHeader7.Width = 100;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(650, 825);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.btn_delete);
+            this.Controls.Add(this.btn_update);
+            this.Controls.Add(this.btn_save);
+            this.Controls.Add(this.btn_clear);
+            this.Controls.Add(this.btn_image);
+            this.Controls.Add(this.tb_image);
+            this.Controls.Add(this.tb_site);
+            this.Controls.Add(this.tb_prize);
+            this.Controls.Add(this.tb_debut);
+            this.Controls.Add(this.tb_agency);
+            this.Controls.Add(this.tb_name);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox tb_name;
+        private System.Windows.Forms.TextBox tb_agency;
+        private System.Windows.Forms.TextBox tb_debut;
+        private System.Windows.Forms.TextBox tb_prize;
+        private System.Windows.Forms.TextBox tb_site;
+        private System.Windows.Forms.TextBox tb_image;
+        private System.Windows.Forms.Button btn_image;
+        private System.Windows.Forms.Button btn_clear;
+        private System.Windows.Forms.Button btn_save;
+        private System.Windows.Forms.Button btn_update;
+        private System.Windows.Forms.Button btn_delete;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.ColumnHeader columnHeader5;
+        private System.Windows.Forms.ColumnHeader columnHeader6;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.ColumnHeader columnHeader7;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Project1
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection conn;
+        SqlCommand cmd;
+        SqlDataReader reader;
+        string conStr;
+        string sql;
+        int eId;
+        string eName, eAgency, eDebut, ePrize, eSite, eImageFileName;
+
+        private void btn_save_Click(object sender, EventArgs e)
+        {
+            eName = tb_name.Text;
+            eAgency = tb_agency.Text;
+            eDebut = tb_debut.Text;
+            ePrize = tb_prize.Text;
+            eSite = tb_site.Text;
+            eImageFileName = tb_image.Text;
+
+            FileStream fs = new FileStream(eImageFileName, FileMode.Open, FileAccess.Read);
+
+            byte[] bImage = new byte[fs.Length];
+            fs.Read(bImage, 0, (int)fs.Length);
+
+            sql = "INSERT INTO enterTable (name, agency, debut, prize, site, imagefilename, image) values ('"
+                + eName + "', '" + eAgency + "', '" + eDebut + "', '" + ePrize + "', '" + eSite + "', '" + eImageFileName + "', @photo);";
+
+            cmd.CommandText = sql;
+            cmd.Parameters.AddWithValue("@photo", bImage);
+            cmd.ExecuteNonQuery();
+
+            fs.Close();
+
+            listView1.Items.Clear();
+
+            sql = "SELECT * FROM enterTable;";
+            cmd.CommandText = sql;
+            reader = cmd.ExecuteReader();
+
+            ListViewItem item;
+
+            while (reader.Read())
+            {
+                eId = reader.GetInt32(0);
+                eName = reader.GetString(1);
+                eAgency = reader.GetString(2);
+                eDebut = reader.GetString(3);
+                ePrize = reader.GetString(4);
+                eSite = reader.GetString(5);
+                eImageFileName = reader.GetString(6);
+
+                item = new ListViewItem(eId.ToString());
+                item.SubItems.Add(eName);
+                item.SubItems.Add(eAgency);
+                item.SubItems.Add(eDebut);
+                item.SubItems.Add(ePrize);
+                item.SubItems.Add(eSite);
+                item.SubItems.Add(eImageFileName);
+
+                listView1.Items.Add(item);
+            }
+            reader.Close();
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_image_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.FileName = "";
+            openFileDialog1.ShowDialog();
+
+            pictureBox1.Image = Image.FromFile(openFileDialog1.FileName);
+            tb_image.Text = openFileDialog1.FileName;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            conStr = "Server=localhost;Database=inhaDB;Trusted_Connection=True;";
+            conn = new SqlConnection(conStr);
+            conn.Open();
+
+            cmd = new SqlCommand();
+            cmd.Connection = conn;
+            sql = "SELECT * FROM enterTable;";
+            cmd.CommandText = sql;
+            reader = cmd.ExecuteReader();
+
+            ListViewItem item;
+
+            while(reader.Read())
+            {
+                eId = reader.GetInt32(0);
+                eName = reader.GetString(1);
+                eAgency = reader.GetString(2);
+                eDebut = reader.GetString(3);
+                ePrize = reader.GetString(4);
+                eSite = reader.GetString(5);
+                eImageFileName = reader.GetString(6);
+
+                item = new ListViewItem(eId.ToString());
+                item.SubItems.Add(eName);
+                item.SubItems.Add(eAgency);
+                item.SubItems.Add(eDebut);
+                item.SubItems.Add(ePrize);
+                item.SubItems.Add(eSite);
+                item.SubItems.Add(eImageFileName);
+
+                listView1.Items.Add(item);
+            }
+            reader.Close();
+        }
+    }
+}
